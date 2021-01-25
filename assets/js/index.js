137c7c32203ce897704b5f69d143a43095d24f37
JORDAN <jordansandoval1819@gmail.com>
2021-01-24 22:46:55 -0600

    Primera version cloud

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,27 @@
-var myCarousel = document.querySelector('#myCarousel')
-var carousel = new bootstrap.Carousel(myCarousel, {
-  interval: 2000,
-  wrap: false
+$(document).ready(function(){
+
+  $('a').click(function(event){  /*scroll*/ 
+    if(this.hash !== ""){
+      event.preventDefault();
+      var hash = this.hash;
+      $('html,body').animate({
+        scrollTop: $(hash).offset().top
+      },8000,function(){
+        window.location.hash = hash
+      })
+    }
+  })
+  $('.carousel').carousel({
+    interval: 2000
+  })
 })
 
+
+
+
+
+/*var myCarousel = document.querySelector('#myCarousel')
+var carousel = new bootstrap.Carousel(myCarousel, {
+  interval: 2000,
+  wrap: false 
+})*/
